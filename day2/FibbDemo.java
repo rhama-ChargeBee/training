03f3227cb601fd000004a4f4ca53deed6fbdaa3f
rhama-ChargeBee <rhama@chargebee.com>
2015-06-19 14:28:07 +0530

    Day2 Fibb

--- day2/FibbDemo.java
+++ day2/FibbDemo.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class FibbDemo{
 	public static void main (String[] args) throws Exception{
 	    	Scanner scan = new Scanner(System.in);
-		int n,i;
+		int n1=0,n2=1, temp, n;
 		int fib=1;
 		System.out.print("Enter the number: ");
 		n= scan.nextInt();
-		for(i=2;i<=n;i++){
-			fib=fib*i;
+		System.out.print("Fibonacci series is ");
+		while(n1<=n){
+			System.out.print(n1+" ");
+			temp=n1;
+			n1=n1+n2;
+			n2=temp;
 		}
-		System.out.println("Fibonacci number is "+fib);
+		System.out.println();
 	}
-
 }
